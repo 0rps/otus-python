4549cc04103cc9fe6cf170b0b5d6c69d53d65755
MogilnikovAB <tuxrps@gmail.com>
2018-05-29 15:12:55 +0300

    [upd] test file structure

---
+++ test_log_analyzer.py
@@ -0,0 +1,21 @@
+import unittest
+import log_analyzer as logan
+
+
+class TestLogAnalyzer(unittest.TestCase):
+
+    def test_regex_filename(self):
+        pass
+
+    def test_regex_message(self):
+        pass
+
+    def test_process_log(self):
+        pass
+
+    def text_report_template_existance(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
