0eaa7c9849c8a294e195af35e70f95ec9c821283
0rps <tuxrps@gmail.com>
2018-07-01 18:19:53 +0300

    [upd] fix tests [upd] change field handle logic

--- hw3/api.py
+++ hw3/api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractclassmethod, ABCMeta
 import json
 import datetime
 import logging
@@ -10,6 +10,7 @@ import uuid
 from weakref import WeakKeyDictionary
 from optparse import OptionParser
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
 import scoring
 
 SALT = "Otus"
@@ -37,8 +38,8 @@ GENDERS = {
     FEMALE: "female",
 }
 
-REQUIRED_FIELD_ERROR = "Field {} is requeired"
-NON_NULLABLE_FIELD_ERROR = "Field {} must be non nullable"
+REQUIRED_FIELD_ERROR = 'Field "{}" is required'
+NON_NULLABLE_FIELD_ERROR = 'Field "{}" couldn\'t be null'
 
 
 class ValidationError(Exception):
@@ -48,151 +49,166 @@ class ValidationError(Exception):
 
 class Field(ABC):
 
-    def __init__(self, required=False, nullable=True):
-        self.required = required
-        self.nullable = nullable
-        self.data = WeakKeyDictionary()
+    def __init__(self, required=False, nullable=False):
+        self._required = required
+        self._nullable = nullable
+        self._data = WeakKeyDictionary()
 
     def __get__(self, instance, owner):
-        return self.data.get(instance, None)
+        return self._data.get(instance, None)
 
     def __set__(self, instance, value):
-        # if not self._is_null(value):
-        self.data[instance] = value
-        # else:
-        #     self.data[instance] = None
-
-    def error_message(self):
-        return 'Field "{}" is not valid'.format(self.field_name)
-
-    def is_null(self, instance):
-        value = self.data.get(instance)
-        return self._is_null(value)
+        if self.is_null_value(value):
+            self._data[instance] = None
+        else:
+            self._data[instance] = value
 
     def is_set(self, instance):
-        return instance in self.data
+        return instance in self._data
 
     def validate(self, instance):
-        if not self.is_set(instance):
-            if self.required:
-                raise ValidationError(REQUIRED_FIELD_ERROR.format(self.field_name))
-
-        value = self.data.get(instance)
-        if self._is_null(value):
-            if not self.nullable:
-                raise ValidationError(NON_NULLABLE_FIELD_ERROR.format(self.field_name))
-
-        self.validate_value(value)
+        if self.is_set(instance):
+            value = self._data.get(instance)
+            if value is not None:
+                self.validate_value(value)
+            elif not self._nullable:
+                msg = NON_NULLABLE_FIELD_ERROR.format(self.field_name)
+                raise ValidationError(msg)
+        elif self._required:
+            msg = REQUIRED_FIELD_ERROR.format(self.field_name)
+            raise ValidationError(msg)
 
     @abstractclassmethod
     def validate_value(self, value):
         return NotImplemented
 
-    @staticmethod
-    def _is_null(value):
-        if value is None:
-            return True
-
-        # TODO: поправить это место
-        if isinstance(value, str) or isinstance(value, dict) or isinstance(value, list):
-            return len(value) == 0
-
-        return False
+    @abstractclassmethod
+    def is_null_value(self, value):
+        return NotImplemented
 
 
 class CharField(Field):
 
     def validate_value(self, value):
-        if self.nullable:
-            if value == '' or value is None:
-                return
         if not isinstance(value, str):
-            raise ValidationError(self.error_message())
+            raise ValidationError('Field "{}"'
+                                  ' is not string'.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None or value == ''
 
 
 class ArgumentsField(Field):
 
     def validate_value(self, value):
-        if self.nullable and value is None:
-            return
         if not isinstance(value, dict):
-            raise ValidationError(self.error_message())
+            raise ValidationError('Field "{}" is not'
+                                  ' dict'.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None or (isinstance(value, dict) and len(value) == 0)
 
 
 class EmailField(CharField):
 
     def validate_value(self, value):
-        if self.nullable and (value is None or value == ''):
-            return
         if not isinstance(value, str):
-            raise ValidationError("{} is not email string".format(self.field_name))
+            raise ValidationError('Field "{}" is '
+                                  'not string'.format(self.field_name))
         splitted = value.split('@')
-        if len(splitted) != 2 or len(splitted[0]) == 0 or len(splitted[1]) == 0:
-            raise ValidationError("{} must be email value with '@' ".format(self.field_name))
+        if len(splitted) != 2 \
+                or len(splitted[0]) == 0 \
+                or len(splitted[1]) == 0:
+            raise ValidationError('Field "{}" must be email '
+                                  'string with one '
+                                  '"@" '.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None or value == ''
 
 
 class PhoneField(Field):
 
     def validate_value(self, value):
-        if self.nullable and (value is None or value == ''):
-            return
         if isinstance(value, int):
             value = str(value)
-        error = "{} must be 11 symbol number/string starting with 7".format(self.field_name)
         if not isinstance(value, str) or len(value) != 11 or value[0] != '7':
-            raise ValidationError(error)
+            raise ValidationError('Field "{}" must '
+                                  'be number/string with 11 symbols '
+                                  'starting with 7'.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None or value == ''
 
 
 class DateField(Field):
 
     def validate_value(self, value):
-        return
+        if not isinstance(value, str):
+            raise ValidationError('Field "{}" is not string')
+
+        try:
+            datetime.datetime.strptime(value, '%d.%m.%Y')
+        except ValueError:
+            raise ValidationError('Field "{}" doesn\'t have '
+                                  'format DD.MM.YYYY'.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None or value == ''
 
 
 class BirthDayField(Field):
 
     def validate_value(self, value):
-        if self.nullable and (value == '' or value is None):
-            return
         if not isinstance(value, str):
-            raise ValidationError(self.error_message())
+            raise ValidationError('Field "{}" is not in string'
+                                  ' format'.format(self.field_name))
 
         try:
             date_value = datetime.datetime.strptime(value, '%d.%m.%Y')
             date_value = date_value.date()
         except ValueError:
-            raise ValidationError(self.error_message())
+            raise ValidationError('Field "{}" doesn\'t have format'
+                                  ' DD.MM.YYYY'.format(self.field_name))
 
         date_delta = datetime.datetime.now().date() - date_value
         if date_delta.days / 365 > 70:
-            raise ValidationError(self.error_message())
+            raise ValidationError('Value of field "{}"'
+                                  ' older than 70'
+                                  ' years'.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None or value == ''
 
 
 class GenderField(Field):
 
     def validate_value(self, value):
-        if self.nullable and value is None:
-            return
-
         if not isinstance(value, int) or value not in (0, 1, 2):
-            raise ValidationError('Field "{}" is not valid'.format(self.field_name))
+            raise ValidationError('Field "{}" is not int'
+                                  ' in range(1,2,3)'.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None
 
 
 class ClientIDsField(Field):
 
     def validate_value(self, value):
-        if self.nullable and value is None:
-            return
-        error = 'Field "{}" is not valid'.format(self.field_name)
         if not isinstance(value, list):
-            raise ValidationError(error)
+            raise ValidationError('Field "{}" is not'
+                                  ' list'.format(self.field_name))
 
         for item in value:
             if not isinstance(item, int):
-                raise ValidationError('Field "{}" is not valid'.format(self.field_name))
+                raise ValidationError('Field "{}" has no'
+                                      ' int values'.format(self.field_name))
+
+    def is_null_value(self, value):
+        return value is None or (isinstance(value, list) and len(value) == 0)
 
 
-class RequestMeta(type):
+class RequestMeta(ABCMeta):
 
     def __init__(mcs, name, bases, attrs):
         super().__init__(name, bases, attrs)
@@ -208,24 +224,23 @@ class Request(metaclass=RequestMeta):
 
     def __init__(self, values):
         self.__errors = None
+        self.__validated = False
+
+        values = values or {}
         for key, value in values.items():
             if hasattr(self, key):
                 setattr(self, key, value)
 
-    def all_data(self):
+    def clean_data(self):
         data = {}
         for field_name, field in self.fields:
             data[field_name] = getattr(self, field_name)
         return data
 
-    def clean_data(self):
-        data = {}
-        for field_name, field in self.fields:
-            if field.is_set(self):
-                data[field_name] = getattr(self, field_name)
-        return data
-
     def is_valid(self):
+        if self.__validated:
+            return self.__errors is None
+
         errors = []
         for field_name, field in self.fields:
             try:
@@ -240,12 +255,13 @@ class Request(metaclass=RequestMeta):
             except ValidationError as ex:
                 errors.append(str(ex))
 
-        self.__errors = errors if len(errors) > 0 else None
+        self.__errors = ';\n'.join(errors) if len(errors) > 0 else None
         return self.__errors is None
 
     def errors(self):
-        return self.__errors.copy()
+        return self.__errors
 
+    @abstractclassmethod
     def validate(self):
         return NotImplemented
 
@@ -255,7 +271,7 @@ class ClientsInterestsRequest(Request):
     date = DateField(required=False, nullable=True)
 
     def validate(self):
-        return True
+        return
 
     @staticmethod
     def handle(request, ctx, store):
@@ -263,10 +279,13 @@ class ClientsInterestsRequest(Request):
 
         if not method.is_valid():
             errors = method.errors()
-            return ';'.join(errors), INVALID_REQUEST
+            logging.error('client_interests request is not valid')
+            return errors, INVALID_REQUEST
 
         ctx['nclients'] = len(method.client_ids)
-        response = {cid: scoring.get_interests(store, cid) for cid in method.client_ids}
+        response = {}
+        for cid in method.client_ids:
+            response[cid] = scoring.get_interests(store, cid)
         return response, OK
 
 
@@ -281,14 +300,16 @@ class OnlineScoreRequest(Request):
     def validate(self):
         data = self.clean_data()
 
-        if 'phone' in data and 'email' in data:
+        if data['phone'] is not None and data['email'] is not None:
             return
-        elif 'first_name' in data and 'last_name' in data:
+        elif data['first_name'] is not None and data['last_name'] is not None:
             return
-        elif 'gender' in data and 'birthday' in data:
+        if data['gender'] is not None and data['birthday'] is not None:
             return
 
-        raise ValidationError('No full pair in request')
+        raise ValidationError('No not empty pair(phone-email, '
+                              'first_name-last_name, '
+                              'gender-birthday) in request')
 
     @staticmethod
     def handle(request, ctx, store):
@@ -296,15 +317,17 @@ class OnlineScoreRequest(Request):
 
         if not method.is_valid():
             errors = method.errors()
+            logging.error('Online score request is not valid')
             return ';'.join(errors), INVALID_REQUEST
 
-        ctx['has'] = method.clean_data().keys()
+        clean_data = method.clean_data()
+        ctx['has'] = [key for key, value in clean_data.items() if value]
 
         if request.is_admin:
+            logging.info('Admin in online_score: returning 42')
             return {'score': 42}, OK
 
-        data = method.all_data()
-        return {'score': scoring.get_score(store, **data)}, OK
+        return {'score': scoring.get_score(store, **clean_data)}, OK
 
 
 class MethodRequest(Request):
@@ -315,48 +338,58 @@ class MethodRequest(Request):
     method = CharField(required=True, nullable=False)
 
     def validate(self):
+        # always valid if all fields are valid
         pass
 
     @property
     def is_admin(self):
         return self.login == ADMIN_LOGIN
 
+    @staticmethod
+    def handle(request, ctx, store):
+        router = {
+            'online_score': OnlineScoreRequest,
+            'clients_interests': ClientsInterestsRequest
+        }
+
+        body = request['body']
+        method_request = MethodRequest(body)
+
+        if not method_request.is_valid():
+            logging.error('Method request is not valid')
+            return method_request.errors(), INVALID_REQUEST
+
+        if not check_auth(method_request):
+            logging.error('Auth failed')
+            return None, FORBIDDEN
+
+        try:
+            concrete_method_class = router[method_request.method]
+        except KeyError:
+            logging.error('Unknown method:'
+                          ' "{}"'.format(method_request.method))
+            return None, NOT_FOUND
+        logging.info('Method request: {}'.format(method_request.method))
+        response, code = concrete_method_class.handle(method_request,
+                                                      ctx, store)
+        return response, code
+
 
 def check_auth(request):
     if request.is_admin:
-        digest = hashlib.sha512((datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).encode('utf-8   ')).hexdigest()
+        digest = hashlib.sha512((datetime.datetime.now().strftime("%Y%m%d%H")
+                                 + ADMIN_SALT).encode('utf-8')).hexdigest()
     else:
-        digest = hashlib.sha512((request.account + request.login + SALT).encode('utf-8')).hexdigest()
+        digest = hashlib.sha512((request.account + request.login
+                                 + SALT).encode('utf-8')).hexdigest()
     if digest == request.token:
         return True
     return False
 
 
-def method_handler(request, ctx, store):
-    router = {
-        'online_score': OnlineScoreRequest,
-        'clients_interests': ClientsInterestsRequest
-    }
-
-    body = request['body']
-
-    method_request = MethodRequest(body)
-
-    if not method_request.is_valid():
-        return ';'.join(method_request.errors()), INVALID_REQUEST
-
-    if not check_auth(method_request):
-        return ERRORS[FORBIDDEN], FORBIDDEN
-
-    concrete_method_class = router[method_request.method]
-    response, code = concrete_method_class.handle(method_request, ctx, store)
-
-    return response, code
-
-
 class MainHTTPHandler(BaseHTTPRequestHandler):
     router = {
-        "method": method_handler
+        "method": MethodRequest
     }
     store = None
 
@@ -370,17 +403,20 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
         try:
             data_string = self.rfile.read(int(self.headers['Content-Length']))
             request = json.loads(data_string)
-        except:
+        except Exception:
             code = BAD_REQUEST
 
         if request:
             path = self.path.strip("/")
-            logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
+            logging.info("%s: %s %s" % (self.path, data_string,
+                                        context["request_id"]))
             if path in self.router:
                 try:
-                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                    response, code = self.router[path].handle(
+                        {"body": request, "headers": self.headers},
+                        context, self.store)
                 except Exception as e:
-                    logging.exception("Unexpected error: %s" % e)
+                    logging.exception("Unexpected error: {}".format(e))
                     code = INTERNAL_ERROR
             else:
                 code = NOT_FOUND
@@ -391,7 +427,9 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
         if code not in ERRORS:
             r = {"response": response, "code": code}
         else:
-            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+            error = response or ERRORS.get(code, "Unknown Error")
+            logging.error("Request errors: {}".format(error))
+            r = {"error": error, "code": code}
         context.update(r)
         logging.info(context)
         self.wfile.write(json.dumps(r))
@@ -404,11 +442,14 @@ if __name__ == "__main__":
     op.add_option("-l", "--log", action="store", default=None)
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
-                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+                        format='[%(asctime)s] %(levelname).1s %(message)s',
+                        datefmt='%Y.%m.%d %H:%M:%S')
     server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
     logging.info("Starting server at %s" % opts.port)
     try:
         server.serve_forever()
     except KeyboardInterrupt:
         pass
+    except Exception as e:
+        logging.exception("Server is shutting down, error: {}".format(e))
     server.server_close()
