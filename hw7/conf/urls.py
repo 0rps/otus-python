07949ea5bec9c69038f30714a15a83ca5bf69e78
0rps <tuxrps@gmail.com>
2018-07-25 23:08:56 +0300

    [upd] Добавлены модели [add] Start templates

--- hw7/conf/urls.py
+++ hw7/conf/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from qa import views as qa_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', qa_views.index)
 ]
--- hw7/qa/models.py
+++ hw7/qa/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from users import models as user_models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    date = models.DateTimeField()
+    rating = models.IntegerField(default=0)
+    answers_count = models.IntegerField(default=0)
+    author = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+
+class Answer(models.Model):
+    body = models.TextField()
+    date = models.DateTimeField()
+    rating = models.IntegerField(default=0)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+
+
+class QuestionLike(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    is_like = models.BooleanField()
+
+
+class AnswerLike(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    is_like = models.BooleanField()
+
